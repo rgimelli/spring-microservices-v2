5ba975fc381438528dff25216e53dca6ecca4e63
Riccardo <riccardo@studiogimelli.com>
2022-11-30 18:48:10 +0100

    updates to make it run with J 17

--- 05.kubernetes/currency-exchange-service/src/main/resources/data.sql
+++ 05.kubernetes/currency-exchange-service/src/main/resources/data.sql
@@ -1,3 +1,4 @@
+create table currency_exchange (id bigint not null, conversion_multiple decimal(19,2), environment varchar(255), currency_from varchar(255), currency_to varchar(255), primary key (id));
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
 values(10001,'USD','INR',65,'');
